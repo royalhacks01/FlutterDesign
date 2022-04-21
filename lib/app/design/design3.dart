2beca2a5494ca942b8a14ba3e7658a9a5373d925
Royalhacks01 <royalvideos95@gmail.com>
2022-04-21 18:07:06 +0530

    git commit

--- lib/app/design/design3.dart
+++ lib/app/design/design3.dart
@@ -5,6 +5,28 @@ class design3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Design 3'),
+        ),
+        elevation: 20.0,
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('Login',
+                style: TextStyle(
+                    height: 5,
+                    fontSize: 50
+                ),
+              )
+            ],
+        ),
+      ),
+    );
   }
 }
