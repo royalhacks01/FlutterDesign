e4c9bfa168107253d25d7ec17fea03922ed53160
Royalhacks01 <royalvideos95@gmail.com>
2022-05-03 14:27:20 +0530

    Full slider screen

--- lib/App/Design/design6.dart
+++ lib/App/Design/design6.dart
@@ -16,132 +16,127 @@ class design6 extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.white,
-          child: Column(
-            children: [
-              Container(
-                color: Colors.blue,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 40, left: 260),
-                  child: Text('Skip',
-                  textAlign: TextAlign.right,
-                  style: TextStyle(
-                    fontSize: 15,
-                  ),
-                  ),
-                ),
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 50, left: 260),
+              child: Text('Skip',
+              textAlign: TextAlign.right,
+              style: TextStyle(
+                fontSize: 15,
               ),
-              SizedBox(
-                height: 600,
-                child: PageView(
-                  controller: controller,
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Container(
-                      color: Colors.white,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 500,
-                            width: 350,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/Image/slide1.jpg'),
-                                fit: BoxFit.contain,
-                              ),
-                              /*shape: BoxShape.circle,*/
+              ),
+            ),
+            SizedBox(
+              height: 600,
+              child: PageView(
+                controller: controller,
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 500,
+                          width: 350,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/Image/slide1.jpg'),
+                              fit: BoxFit.contain,
                             ),
+                            /*shape: BoxShape.circle,*/
                           ),
-                          Text(dataString,
+                        ),
+                        Text(dataString,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 500,
+                          width: 350,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/Image/slide2.jpg'),
+                              fit: BoxFit.contain,
+                            ),
+                            /*shape: BoxShape.circle,*/
+                          ),
+                        ),
+                        Text(dataString,
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontSize: 20,
                             color: Colors.black,
                           ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      color: Colors.white,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 500,
-                            width: 350,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/Image/slide2.jpg'),
-                                fit: BoxFit.contain,
-                              ),
-                              /*shape: BoxShape.circle,*/
+                  ),
+                  Container(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 500,
+                          width: 350,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/Image/slide3.jpg'),
+                              fit: BoxFit.contain,
                             ),
+                            /*shape: BoxShape.circle,*/
                           ),
-                          Text(dataString,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                            ),
+                        ),
+                        Text(dataString,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      color: Colors.white,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 500,
-                            width: 350,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/Image/slide3.jpg'),
-                                fit: BoxFit.contain,
-                              ),
-                              /*shape: BoxShape.circle,*/
-                            ),
-                          ),
-                          Text(dataString,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              SmoothPageIndicator(
-                controller: controller,
-                count: 3,
-                effect: SlideEffect(
-                  radius: 8.0,
-                  dotWidth: 8.0,
-                  dotHeight: 8.0,
-                  dotColor: Colors.grey,
-                  activeDotColor: Colors.green,
-                ),
+            ),
+            SmoothPageIndicator(
+              controller: controller,
+              count: 3,
+              effect: SlideEffect(
+                radius: 8.0,
+                dotWidth: 8.0,
+                dotHeight: 8.0,
+                dotColor: Colors.grey,
+                activeDotColor: Colors.green,
               ),
-            ],
-          ),
-
+            ),
+          ],
         ),
+
       ),
     );
   }
